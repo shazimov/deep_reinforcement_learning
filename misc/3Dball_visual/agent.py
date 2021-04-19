cf3e18356ede2a8d053e993e009fc42792a95141
unknown <shazimov@protonmail.com>
2021-04-19 11:55:48 -0700

    3Dball_visual added prioritized replay

--- misc/3Dball_visual/agent.py
+++ misc/3Dball_visual/agent.py
@@ -57,18 +57,45 @@ class Agent():
         # Initialize time step (for updating every self.update_every steps)
         self.t_step = 0
 
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        
+        next_state_torch = torch.from_numpy(next_state).float().to(self.device)
+        reward_torch = torch.from_numpy(np.array(reward)).float().to(self.device)
+        done_torch = torch.from_numpy(np.array(done).astype(np.uint8)).float().to(self.device)
+        state_torch = torch.from_numpy(state).float().to(self.device)
+        action_torch = torch.from_numpy(action).float().to(self.device)
+        
+        self.actor_target.eval()
+        self.critic_target.eval()
+        self.critic_local.eval()
+        with torch.no_grad():
+            action_next = self.actor_target(next_state_torch)
+            Q_target_next = self.critic_target(next_state_torch, action_next)
+            Q_target = reward_torch + (self.gamma * Q_target_next * (1 -done_torch))
+            Q_expected = self.critic_local(state_torch, action_torch)
+        self.actor_local.train()
+        self.critic_target.train()
+        self.critic_local.train()
+        
+        #Error used in prioritized replay buffer
+        error = (Q_expected - Q_target).squeeze().cpu().data.numpy()
+        
+        #Adding experiences to prioritized replay buffer
+        #for i in np.arange(len(reward)):
+        self.memory.add(error, state, action, reward, next_state, done)
+
     def step(self, state, action, reward, next_state, done):
-        """Save experience in replay memory, and use random sample from buffer to learn."""
+        """Save experience in replay memory."""
         # Save experience / reward
-        #for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
-        self.memory.add(state, action, reward, next_state, done)
+        self.add(state, action, reward, next_state, done)
 
         # Learn, if enough samples are available in memory
         self.t_step = (self.t_step + 1) % self.update_every
         if self.t_step == 0:
             if len(self.memory) > self.batch_size:
-                experiences = self.memory.sample()
-                self.learn(experiences, self.gamma)
+                experiences, idxs, is_weights = self.memory.sample()
+                self.learn(experiences, idxs, is_weights)
 
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
@@ -84,7 +111,7 @@ class Agent():
     def reset(self):
         self.noise.reset()
 
-    def learn(self, experiences, gamma):
+    def learn(self, experiences, idxs, is_weights):
         """Update policy and value parameters using given batch of experience tuples.
         Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
         where:
@@ -94,7 +121,6 @@ class Agent():
         Params
         ======
             experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
-            gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones = experiences
 
@@ -103,13 +129,19 @@ class Agent():
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
         # Compute Q targets for current states (y_i)
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
         # Compute critic loss
         Q_expected = self.critic_local(states, actions)
-        critic_loss = F.mse_loss(Q_expected, Q_targets)
+        #critic_loss = F.mse_loss(Q_expected, Q_targets)
+        critic_loss = (torch.from_numpy(is_weights).float().to(self.device) * F.mse_loss(Q_expected, Q_targets)).mean()
+
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
+
+        #gradient clipping
+        #torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1) 
+
         self.critic_optimizer.step()
 
         # ---------------------------- update actor ---------------------------- #
@@ -124,6 +156,16 @@ class Agent():
         # ----------------------- update target networks ----------------------- #
         self.soft_update(self.critic_local, self.critic_target, self.tau)
         self.soft_update(self.actor_local, self.actor_target, self.tau)
+
+        #.......................update priorities in prioritized replay buffer.......#
+        #Calculate errors used in prioritized replay buffer
+        errors = (Q_expected-Q_targets).squeeze().cpu().data.numpy()
+        
+        # update priority
+        for i in range(self.batch_size):
+            idx = idxs[i]
+            self.memory.update(idx, errors[i])
+
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
